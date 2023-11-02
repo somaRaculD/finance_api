6ae4225de6aa3c4d48e407605a72f4a0fb8c9d89
Your Name <you@example.com>
2023-11-02 07:18:36 -0300

    adicionado variaveis de ambiente nos arquivos

--- src/data-souce.providers.ts
+++ src/data-souce.providers.ts
@@ -3,23 +3,29 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = [{
     provide: 'DATA_SOURCE',
-    useFactory: async () =>{
-    const dataSource = new DataSource({
-        type: "postgres",
-        host: "db-postgresql-nyc3-79668-do-user-14820194-0.b.db.ondigitalocean.com",
-        port: 2506,
-        username: "postgres_user_m",
-        password: "AVNS_NxV0LO3QjYqqE54ai6A",
-        database: "finance",
-        url: 'postgresql://postgres_user_m:AVNS_NxV0LO3QjYqqE54ai6A@db-postgresql-nyc3-79668-do-user-14820194-0.b.db.ondigitalocean.com:25061/primeira_conexao?sslmode=require',
-        entities: [
-            __dirname + "/entity/*{.ts,.js}"
-        ],
-        synchronize: false
-    });
-    return dataSource.initialize();
-}
+    useFactory: async () => {
+        const dataSource = new DataSource({
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            "ssl": true,
+            "extra": {
+                "ssl": {
+                    "rejectUnauthorized": false
+                }
+            },
+            url: process.env.DB_URL,
+            entities: [
+                __dirname + "/entity/*{.ts,.js}"
+            ],
+            synchronize: false
+        });
+        return dataSource.initialize();
+    }
 }];
 
-
 export default AppDataSource;
+
