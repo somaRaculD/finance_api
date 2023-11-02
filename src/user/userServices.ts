c25c9b9a53c49a72a71b80cccf791446fce2b022
Your Name <you@example.com>
2023-11-02 07:20:42 -0300

    instalado bcrypt e colocado senha hasheada

--- src/user/userServices.ts
+++ src/user/userServices.ts
@@ -5,6 +5,7 @@ import { UserDto } from "./user.interface";
 import { SessionDto } from "./session.interface";
 import { sign, verify } from "jsonwebtoken";
 import { Request } from "express";
+import { compare, hash } from 'bcrypt'
 
 export interface Token {
     token: String;
@@ -35,7 +36,16 @@ export class UserService {
 
     async create(createUser: UserDto): Promise<any> {
         try {
-            const user = this.userRepository.create(createUser)
+            const { password, confPassword } = createUser;
+            if (!password.match(confPassword)) {
+                throw new NotFoundException('Senha e confirmação de senha estão divergentes');
+            }
+
+            const passwordHashed:any = await hash(password, 12, (err, hash) => {
+                return hash;
+            });
+
+            const user = this.userRepository.create({...createUser, password: passwordHashed || ''})
             return await this.userRepository.save(user)
         } catch (err) {
             throw new NotFoundException(`user not created`)
@@ -44,7 +54,12 @@ export class UserService {
 
     async createSession(createSession: SessionDto): Promise<Token> {
         try {
-            const user = await this.userRepository.findOne({ where: { email: createSession.email, password: createSession.password } });
+            const user = await this.userRepository.findOne({ where: { email: createSession.email } });
+            const passwordHashed = await hash(createSession.password, 12);
+            const match = await compare(passwordHashed, user.password);
+            if(!match) {
+                throw new HttpException('Senha inválida!', HttpStatus.UNAUTHORIZED);
+            }
             const session = sign({ data: { id: user.id } }, 'shh', { expiresIn: Math.floor(Date.now() / 1000) + (60 * 60 * 3) });
             return { token: session, dateExp: Math.floor(Date.now() / 1000) + (60 * 60 * 3), user: user.name };
         } catch (err) {
